5a284e404b9e6fe9d49d82533fa8946acd43ba99
Diego Espinoza <diespinozas@gmail.com>
2018-10-11 23:03:28 -0700

    add farebase login functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { config } from "./firebase.config";
 const firebase = require("firebase");
 
 firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if (firebaseUser) {
+    console.log(firebaseUser);
+  } else {
+    console.log("not loged in");
+  }
+});
+
 class App extends Component {
   constructor() {
     super();
@@ -11,12 +20,44 @@ class App extends Component {
       email: "",
       password: ""
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.singUp = this.singUp.bind(this);
   }
+
   handleChange(e) {
     if (e.target.id === "txtEmail") this.setState({ email: e.target.value });
     if (e.target.id === "txtPassword")
       this.setState({ password: e.target.value });
   }
+
+  logIn() {
+    // const auth = firebase.auth();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      // .then(() => this.setState({ email: "", password: "" }))
+      .catch(e => {
+        // if (e.code === "auth/user-not-found") console.log("please sing up");
+        // if(e.code )
+        console.log(e);
+      });
+    // this.setState({ email: "", password: "" });
+  }
+
+  singUp() {
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      this.state.email,
+      this.state.password
+    );
+    promise.catch(e => console.log(e.message));
+  }
+
+  logOut() {
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <div>
@@ -32,9 +73,9 @@ class App extends Component {
         <input
           id="txtPassword"
           type="password"
+          placeholder="Password"
           onChange={this.handleChange}
           value={this.state.password}
-          placeholder="Password"
         />
         <br />
         <button id="btnLogin" onClick={this.logIn}>
