e5a65da02c2df75d365d869bc4d19896d12bbf36
Diego Espinoza <diespinozas@gmail.com>
2018-10-22 23:48:34 -0400

    add styling to Navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import AuthUserContext from "./AuthUserContext";
 import SignOutButton from "./SignOut";
 import * as routes from "../constants/routes";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 const Navigation = () => (
   <AuthUserContext.Consumer>
@@ -12,31 +15,37 @@ const Navigation = () => (
 );
 
 const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={routes.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={routes.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={routes.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Button component={Link} to={routes.LANDING} color="inherit">
+          Landing
+        </Button>
+        <Button component={Link} to={routes.HOME} color="inherit">
+          Home
+        </Button>
+        <Button component={Link} to={routes.ACCOUNT} color="inherit">
+          Account
+        </Button>
+        <SignOutButton />
+      </Toolbar>
+    </AppBar>
+  </div>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={routes.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={routes.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Button component={Link} to={routes.LANDING} color="inherit">
+          Landing
+        </Button>
+        <Button component={Link} to={routes.SIGN_IN} color="inherit">
+          Sign In
+        </Button>
+      </Toolbar>
+    </AppBar>
+  </div>
 );
 
 export default Navigation;
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 import { auth } from "../firebase";
 
 const SignOutButton = () => (
-  <button type="button" onClick={auth.doSignOut}>
+  <Button color="inherit" onClick={auth.doSignOut}>
     Sign Out
-  </button>
+  </Button>
 );
 
 export default SignOutButton;
