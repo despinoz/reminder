f61ab1646e227fe2b2da1ebe196e8bf108598085
Diego Espinoza <diespinozas@gmail.com>
2018-10-23 23:25:11 -0400

    add styling to Landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 const LandingPage = () => (
   <div>
-    <h1>Landing Page</h1>
+    <Typography component="h2" variant="display3" gutterBottom>
+      Bienvenido a Reminder!
+    </Typography>
   </div>
 );
 
